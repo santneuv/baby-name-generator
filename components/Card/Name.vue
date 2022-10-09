4eb8cad66abfc03c3255fc113b384d6dd83c46f0
onur güler <onurgler1@gmail.com>
2022-10-09 19:19:47 +0300

    first commit

---
+++ components/Card/Name.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface NameProps {
+  name: string;
+  index: number;
+}
+
+const props = defineProps<NameProps>();
+
+const emit = defineEmits(["remove"]);
+
+const removeName = () => {
+  emit("remove", props.index);
+};
+</script>
+
+<template>
+  <div class="card">
+    <h4>{{ name }}</h4>
+    <p @click="removeName">X</p>
+  </div>
+</template>
+
+<style lang="scss">
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+  .card {
+    background-color: rgb(27, 60, 138);
+    width: 30%;
+    color: #fff;
+    border-radius: 1rem;
+    padding: 0.1rem;
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+    position: relative;
+    p {
+      position: absolute;
+      top: -20%;
+      left: 90.5%;
+      cursor: pointer;
+      color: rgba(255, 255, 255, 0.178);
+    }
+  }
+}
+</style>
---
+++ components/Option.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { Gender, Popularity, Length } from "~~/data";
+
+interface OptionProps {
+  option: {
+    title: string;
+    category: string;
+    buttons: Gender[] | Popularity[] | Length[];
+  };
+  options: {
+    gender: Gender;
+    popularity: Popularity;
+    lenght: Length;
+  };
+}
+
+const props = defineProps<OptionProps>();
+</script>
+
+<template>
+  <div class="option-container">
+    <h4>{{ option.title }}</h4>
+    <div class="option-buttons">
+      <button
+        v-for="value in option.buttons"
+        :key="value"
+        class="option"
+        :class="options[option.category] === value && 'option-active'"
+        @click="options[option.category] = value"
+      >
+        {{ value }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+  .option-container {
+    margin-bottom: 2rem;
+    .option {
+      appearance: none;
+      background-color: #fff;
+      outline: 0.15rem solid rgb(249, 87, 89);
+      border: none;
+      padding: 0.75rem;
+      width: 12rem;
+      font-size: 1rem;
+      color: rgb(27, 60, 138);
+      cursor: pointer;
+      font-weight: 200;
+      &:first-child {
+        border-radius: 1rem 0 0 1rem;
+      }
+      &:last-child {
+        border-radius: 0 1rem 1rem 0;
+      }
+    }
+    .option-active {
+      background-color: rgb(249, 87, 89);
+      color: #fff;
+    }
+  }
+}
+</style>
